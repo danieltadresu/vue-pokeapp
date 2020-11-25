de4f7296e6d41b22e503c3b325169362465a4492
Dani <daniaxasabraxas@gmail.com>
2020-11-25 20:23:58 -0300

    Removing prebuild component and starting to build the app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Vue PokeApp</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {}
 </script>
 
 <style>
