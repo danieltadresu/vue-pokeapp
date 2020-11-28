b261c6ee547175017f7f5e20bb48de181dbf7ac8
Dani <daniaxasabraxas@gmail.com>
2020-11-27 21:46:10 -0300

    Added BaseCard in main.js file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import store from './store/index.js';
 import App from './App.vue';
+import BaseCard from './components/ui/BaseCard.vue';
 const app = createApp(App);
 app.use(store);
+app.component('base-card', BaseCard);
 app.mount('#app');
