d7a28cf5e158ffd0baa5695f775826447eeae326
Dani <daniaxasabraxas@gmail.com>
2020-11-28 18:23:47 -0300

    Edited BaseCard, added a prop to change the style

--- src/components/ui/BaseCard.vue
+++ src/components/ui/BaseCard.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="card">
+  <div v-bind:class="mode">
     <slot> </slot>
   </div>
 </template>
 
+<script>
+export default {
+  props: ['mode']
+}
+</script>
+
 <style scoped>
-.card {
+.card-list {
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
