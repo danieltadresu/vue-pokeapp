c7f3de852cd622204ef61310b874e9da16b92f1a
Dani <daniaxasabraxas@gmail.com>
2020-11-28 15:47:12 -0300

    Fetching all pokemon

--- src/pages/pokemon/PokemonDetail.vue
+++ src/pages/pokemon/PokemonDetail.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>PokemonDetail file</h1>
+  <h1>PokemonDetail</h1>
+  <p> {{ fetchPokemon }} </p>
 </template>
+
+<script>
+  export default {
+    created() {
+      this.$store.dispatch('loadPokemonData');
+    },
+    computed: {
+      fetchPokemon() {
+        return this.$store.getters.pokemon;
+      }
+    }
+  }
+</script>
