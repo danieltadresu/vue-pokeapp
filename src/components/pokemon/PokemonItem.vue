9cef522c3a829d3941c9afb48ff2d6609f865ad1
Dani <daniaxasabraxas@gmail.com>
2020-11-27 21:45:19 -0300

    Fetching Abilities

--- src/components/pokemon/PokemonItem.vue
+++ src/components/pokemon/PokemonItem.vue
@@ -1,10 +1,26 @@
 <template>
   <li>
     <base-card>
-      <h2>{{ pokemonTitle }}</h2>
 
-      <p>{{ pokemonImage }}</p>
-      <p>{{ pokemonAbilities }}</p>
+      <header>
+        <h2>{{ pokemonTitle }}</h2>
+      </header>
+
+      <section>
+        <img v-bind:src="pokemonImage">
+      </section>
+
+      <footer>
+        <h3>Abilities</h3>
+        <nav>
+          <ul>
+            <li v-for="(ability, index) in pokemonAbilities" v-bind:key="index">
+              <h3>{{ ability.toUpperCase() }}</h3>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+
     </base-card>
   </li>
 </template>
@@ -14,7 +30,7 @@
     props: ['pokemonId', 'pokemonName', 'pokemonImage', 'pokemonAbilities'],
     computed: {
       pokemonTitle() {
-        return this.pokemonName.toUpperCase();
+        return this.pokemonId + ' ' + this.pokemonName.toUpperCase();
       }
     }
   }
@@ -25,4 +41,21 @@
     margin: 0;
     padding: 0;
   }
+
+  header, section, footer {
+    margin: 2rem 0;
+    text-align: center;
+  }
+
+  nav {
+    border: 1px solid black;
+    display: inline-block;
+    padding: 0 4rem;
+    border: 1px solid #eee;
+    border-radius: 12px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  }
+
+
+
 </style>
