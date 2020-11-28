e12f8c89199226db43bcda10353bb03bb8bb3626
Dani <daniaxasabraxas@gmail.com>
2020-11-27 23:05:53 -0300

    Changed some styles and added MQ

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,6 +1,14 @@
 <template>
   <header>
     <h1>{{headerTitle}}</h1>
+    <ul>
+      <li>
+        POKEMON LIST
+      </li>
+      <li>
+        POKEMON DETAILS
+      </li>
+    </ul>
   </header>
 </template>
 
@@ -11,7 +19,32 @@ export default {
 </script>
 
 <style scoped>
- h1 {
-  text-align: center;
- }
+
+header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+h1 {
+  display: inline-block;
+}
+
+ul, li {
+  display: inline-block;
+  list-style-type: none;
+  margin: 0 .5rem;
+  padding: 0;
+}
+
+@media screen and (max-width: 768px) {
+  header {
+    flex-direction: column;
+  }
+}
+
+
+
 </style>
