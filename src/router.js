639a2165f6797af71d90c1ebf43fcbc01c406571
Dani <daniaxasabraxas@gmail.com>
2020-11-28 21:56:45 -0300

    Deleting detail router

--- src/router.js
+++ src/router.js
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import PokemonList from './pages/pokemon/PokemonList.vue';
-import PokemonDetail from './pages/pokemon/PokemonDetail.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/pokemon' },
-    { path: '/pokemon', component: PokemonList },
-    { path: '/pokemon/:pokemonId', component: PokemonDetail }
+    { path: '/pokemon', component: PokemonList }
   ]
 });
 
